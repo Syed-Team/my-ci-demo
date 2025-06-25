e5f9a37fd6b1ba4b216bef960da17d3541f8ab17
syed Git <syed@git.com>
2025-06-25 10:14:03 +0530

    after change i just start deploy

--- appli.py
+++ appli.py
@@ -1,2 +1,10 @@
-print('Hello, CI/CD!')
+from flask import Flask
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, CI/CD from Render!"
+
+if __name__ == "__main__":
+    app.run()
